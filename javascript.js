9db879e436a353c67e00d624b332d89e85052755
DonovanBurrola <donovan.burrola@student.cart.org>
2023-02-09 15:35:44 -0800

    gave javascript its own file

---
+++ javascript.js
@@ -0,0 +1,494 @@
+        // let job =["Acountant", 40000]
+        // index.getElementById("job").innerHTML = string;
+        // let job='accountant';
+        // document.getElementById("job").innerHTML = job;
+        // let data = require(EECU-data.js).data;
+
+        // let x=10;
+        // x=null;
+        // function send(order){
+        //     // document.getElementById("fedDeduction").innerHTML ='ssssssssssssss'
+        //     x=parseInt(order)
+        // }
+        let jobOrder = null;
+        function accountant(){
+            jobOrder = 0
+            deductions()
+        }
+        function tractor(){
+            jobOrder = 1
+            deductions()
+        }
+        function agriEng(){
+            jobOrder = 2;
+            deductions()
+        }
+        function arch(){
+            jobOrder = 3;
+            deductions()
+        }
+        function autoTech(){
+            jobOrder = 4;
+            deductions()
+        }
+        function aviTech(){
+            jobOrder = 5;
+            deductions()
+        }
+        function bio(){
+            jobOrder = 6;
+            deductions()
+        }
+        function bus(){
+            jobOrder = 7;
+            deductions()
+        }
+        function businessDev(){
+            jobOrder = 8;
+            deductions()
+        }
+        function businessMan(){
+            jobOrder = 9;
+            deductions()
+        }
+        function cncManu(){
+            jobOrder = 10;
+            deductions()
+        }
+        function carpen(){
+            jobOrder = 11;
+            deductions()
+        }
+        function chef(){
+            jobOrder = 12;
+            deductions()
+        }
+        function chem(){
+            jobOrder = 13;
+            deductions()
+        }
+        function civEng(){
+            jobOrder = 14;
+            deductions()
+        }
+        function comDriver(){
+            jobOrder = 15;
+            deductions()
+        }
+        function comTech(){
+            jobOrder = 16;
+            deductions()
+        }
+        function envSci(){
+            jobOrder = 17;
+            deductions()
+        }
+        function corOff(){
+            jobOrder = 18;
+            deductions()
+        }
+        function cos(){
+            jobOrder = 19;
+            deductions()
+        }
+        function bankMan(){
+            jobOrder = 20;
+            deductions()
+        }
+        function daycare(){
+            jobOrder = 21;
+            deductions()
+        }
+        function dentist(){
+            jobOrder = 22;
+            deductions()
+        }
+        function detective(){
+            jobOrder = 23;
+            deductions()
+        }
+        function dieselTech(){
+            jobOrder = 24;
+            deductions()
+        }
+        function doc(){
+            jobOrder = 25;
+            deductions()
+        }
+        function elct(){
+            jobOrder = 26;
+            deductions()
+        }
+        function elctEng(){
+            jobOrder = 27;
+            deductions()
+        }
+        function emt(){
+            jobOrder = 28;
+            deductions()
+        }
+        function pgeMan(){
+            jobOrder = 29;
+            deductions()
+        }
+        function eng(){
+            jobOrder = 30;
+            deductions()
+        }
+        function fashDes(){
+            jobOrder = 31;
+            deductions()
+        }
+        function fireFight(){
+            jobOrder = 32;
+            deductions()
+        }
+        function forestRan(){
+            jobOrder = 33;
+            deductions()
+        }
+        function mediaDes(){
+            jobOrder = 34;
+            deductions()
+        }
+        function hVac(){
+            jobOrder = 35;
+            deductions()
+        }
+        function highway(){
+            jobOrder = 36;
+            deductions()
+        }
+        function homeInspect(){
+            jobOrder = 37;
+            deductions()
+        }
+        function indMech(){
+            jobOrder = 38;
+            deductions()
+        }
+        function intDes(){
+            jobOrder = 39;
+            deductions()
+        }
+        function invAna(){
+            jobOrder = 40;
+            deductions()
+        }
+        function labTech(){
+            jobOrder = 41;
+            deductions()
+        }
+        function landScape(){
+            jobOrder = 42;
+            deductions()
+        }
+        function law(){
+            jobOrder = 43;
+            deductions()
+        }
+        function marketing(){
+            jobOrder = 44;
+            deductions()
+        }
+        function communication(){
+            jobOrder = 45;
+            deductions()
+        }
+        function medRepTech(){
+            jobOrder = 46;
+            deductions()
+        }
+        function milit(){
+            jobOrder = 47;
+            deductions()
+        }
+        function nurse(){
+            jobOrder = 48;
+            deductions()
+        }
+        function nutr(){
+            jobOrder = 49;
+            deductions()
+        }
+        function ocean(){
+            jobOrder = 50;
+            deductions()
+        }
+        function pastor(){
+            jobOrder = 51;
+            deductions()
+        }
+        function pgeTech(){
+            jobOrder = 52;
+            deductions()
+        }
+        function pharm(){
+            jobOrder = 53;
+            deductions()
+        }
+        function photo(){
+            jobOrder = 54;
+            deductions()
+        }
+        function physTher(){
+            jobOrder = 55;
+            deductions()
+        }
+        function pilot(){
+            jobOrder = 56;
+            deductions()
+        }
+        function plumb(){
+            jobOrder = 57;
+            deductions()
+        }
+        function police(){
+            jobOrder = 58;
+            deductions()
+        }
+        function principal(){
+            jobOrder = 59;
+            deductions()
+        }
+        function probOff(){
+            jobOrder = 60;
+            deductions()
+        }
+        function psyc(){
+            jobOrder = 61;
+            deductions()
+        }
+        function retail(){
+            jobOrder = 62;
+            deductions()
+        }
+        function socialWork(){
+            jobOrder = 63;
+            deductions()
+        }
+        function solarTech(){
+            jobOrder = 64;
+            deductions()
+        }
+        function teach(){
+            jobOrder = 65;
+            deductions()
+        }
+        function ups(){
+            jobOrder = 66;
+            deductions()
+        }
+        function vet(){
+            jobOrder = 67;
+            deductions()
+        }
+        function weld(){
+            jobOrder = 68;
+            deductions()
+        }
+        function windTech(){
+            jobOrder = 69;
+            deductions()
+        }
+
+
+        
+
+        let data = [
+            [ 'Accountant', 55650 ],
+            [ 'Advance Tractor/Trailer Driver', 53550 ],
+            [ 'Agricultural Engineer', 56700 ],
+            [ 'Architect', 53550 ],
+            [ 'Auto Tech/Mechanic', 49350 ],
+            [ 'Aviation Tech. Mechanic', 50400 ],
+            [ 'Biologist', 54600 ],
+            [ 'Bus Driver', 37800 ],
+            [ 'Business Development Officer', 54600 ],
+            [ 'Business Manager Hotel etc.', 61950 ],
+            [ 'CNC Manufacturing', 80850 ],
+            [ 'Carpenter', 47250 ],
+            [ 'Chef', 52500 ],
+            [ 'Chemist', 56700 ],
+            [ 'Civil Engineering Technician', 68250 ],
+            [ 'Commercial Driver', 51450 ],
+            [ 'ComputerTechnician', 46200 ],
+            [ 'Conserv./Environ. Science', 72450 ],
+            [ 'Correctional Officer', 48300 ],
+            [ 'Cosmetologist', 36750 ],
+            [ 'Credit Union/Bank Manager', 61950 ],
+            [ 'Daycare Director', 37800 ],
+            [ 'Dentist', 115500 ],
+            [ 'Detective', 60900 ],
+            [ 'Diesel Tech/Mechanic', 55650 ],
+            [ 'Doctor/Physician', 147000 ],
+            [ 'Electrician', 54600 ],
+            [ 'Electronic Engineer', 75600 ],
+            [ 'EMT', 34650 ],
+            [ 'Energy Management PG&E', 106050 ],
+            [ 'Engineer', 72450 ],
+            [ 'Fashion Designer', 63000 ],
+            [ 'Fire Fighter', 49350 ],
+            [ 'Forest Ranger', 52500 ],
+            [ 'Graphic/Media Designer', 58800 ],
+            [ 'H/C HVAC', 63000 ],
+            [ 'Highway Patrol', 84000 ],
+            [ 'Home Inspector', 56700 ],
+            [ 'Industrial Mechanic', 46200 ],
+            [ 'Interior Designer', 49350 ],
+            [ 'Investment Analyst', 66150 ],
+            [ 'Lab Technician', 42000 ],
+            [ 'Landscaper Horticulture', 48300 ],
+            [ 'lawyer', 86100 ],
+            [ 'Marketing/Sales Manager', 58800 ],
+            [ 'Media/Communications', 45150 ],
+            [ 'Medical RepairTech.', 52500 ],
+            [ 'Military', 55650 ],
+            [ 'Nuse', 66150 ],
+            [ 'Nutitionist', 45150 ],
+            [ 'Oceanographer', 69300 ],
+            [ 'Pastor', 50400 ],
+            [ 'PGBEATT Technician', 78750 ],
+            [ 'Pharmacist', 105000 ],
+            [ 'Photographer', 45150 ],
+            [ 'Physical Therapist', 72450 ],
+            [ 'Pilot Commercial', 78750 ],
+            [ 'Plumber', 52500 ],
+            [ 'Police Officer', 53550 ],
+            [ 'Principal', 93450 ],
+            [ 'Probation Officer', 44100 ],
+            [ 'Psychologist', 77700 ],
+            [ 'Retail Sales Associate', 34650 ],
+            [ 'Social Worker', 50400 ],
+            [ 'Solar Energy Tech.', 53550 ],
+            [ 'Teacher', 52500 ],
+            [ 'UPS/Fed Ex Driver', 68250 ],
+            [ 'Veterinarian', 82950 ],
+            [ 'Welder/Metal Specialist', 47250 ],
+            [ 'Wind EnergyTechnician', 56700 ]
+        ]
+
+        // x=data[10];
+        // let accountant= document.getElementById('accountant');
+        // document.getElementById("fedDeduction").innerHTML ='$'+data[x][1]*.12;
+        function deductions(){
+            document.getElementById("job").innerHTML =data[jobOrder][0];
+            document.getElementById("fedDeduction").innerHTML ='$'+Math.round(data[jobOrder][1]*.12/12);
+            document.getElementById("stateDeduction").innerHTML ='$'+Math.round(data[jobOrder][1]*.072/12);
+            document.getElementById("socialDeduction").innerHTML ='$'+Math.round(data[jobOrder][1]*.062/12);
+            document.getElementById("medDeduction").innerHTML ='$'+Math.round(data[jobOrder][1]*.0145/12);
+            document.getElementById("disabilityDeduction").innerHTML ='$'+Math.round(data[jobOrder][1]*.01/12);
+            document.getElementById("retireDeduction").innerHTML ='$'+Math.round(data[jobOrder][1]*.05/12);
+            
+            let GA = data[jobOrder][1];
+            let GM = GA/12;
+            let YD = Math.round(data[jobOrder][1]*.3625 + 2160);
+            let MD = Math.round(data[jobOrder][1]*.3625/12 + 180);
+            let NY = GA-YD;
+            let NM = GM-MD;
+            let HP = Math.round(GM*.33)
+            document.getElementById("grossYear").innerHTML = '$'+ GA;
+            document.getElementById("grossMonth").innerHTML = '$'+ GM;
+            document.getElementById("deductYear").innerHTML = '$'+ YD;
+            document.getElementById("deductMonth").innerHTML = '$'+ MD;
+            document.getElementById("netYear").innerHTML = '$'+ NY;
+            document.getElementById("netMonth").innerHTML = '$'+ NM;
+            document.getElementById("houseRange").innerHTML = '$'+ HP;
+            let CB=NM;            
+        }
+        
+        // let transactions=[];
+
+
+        const formOne = document.querySelector('#transactionOne');
+
+        const formTwo = document.querySelector('#transactionTwo');
+
+        const formThree = document.querySelector('#transactionThree');
+
+        const formFour = document.querySelector('#transactionFour');
+
+        const formFive = document.querySelector('#transactionFive');
+
+        const formSix = document.querySelector('#transactionSix');
+
+        const formSeven = document.querySelector('#transactionSeven');
+
+        const formEight = document.querySelector('#transactionEight');
+
+        const formNine = document.querySelector('#transactionNine');
+
+        const formTen = document.querySelector('#transactionTen');
+
+
+
+
+        const inputDetailsOne = document.querySelector('#transactionDetailsOne');
+        const inputAmountOne = document.querySelector('#amountExchangedOne');
+
+
+        const inputDetailsTwo = document.querySelector('#transactionDetailsTwo');
+
+        const inputDetailsThree = document.querySelector('#transactionDetailsThree');
+
+        const inputDetailsFour = document.querySelector('#transactionDetailsFour');
+
+        const inputDetailsFive = document.querySelector('#transactionDetailsFive');
+
+        const inputDetailsSix = document.querySelector('#transactionDetailsSix');
+
+        const inputDetailsSeven = document.querySelector('#transactionDetailsSeven');
+
+        const inputDetailsEight = document.querySelector('#transactionDetailsEight');
+
+        const inputDetailsNine = document.querySelector('#transactionDetailsNine');
+
+        const inputDetailsTen = document.querySelector('#transactionDetailsTen');
+
+        let balance;
+        
+        let transactionOne=[];
+        
+
+
+
+
+        inputDetailsOne.addEventListener('blur', (event) => {
+            const DetailsOne = inputDetailsOne.value;
+            console.log(DetailsOne);
+            if (isNaN(DetailsOne)){
+                document.getElementById('amountExchangedOne').disabled = false;
+                transactionOne.pop()
+                transactionOne.push(DetailsOne)
+                console.log(transactionOne)
+
+            } else{
+                document.getElementById('amountExchangedOne').disabled = true;
+        }
+         });
+         inputAmountOne.addEventListener('blur', (event) => {
+            const amountOne = inputAmountOne.value;
+            transactionOne.push(amountOne)
+            console.log(amountOne);
+        });
+
+        inputDetailsTwo.addEventListener('blur', (event) => {const DetailsTwo = inputDetailsTwo.value; console.log(DetailsTwo)});
+
+        inputDetailsThree.addEventListener('blur', (event) => {const DetailsThree = inputDetailsThree.value;console.log(DetailsThree)});
+        
+        inputDetailsFour.addEventListener('blur', (event) => {const DetailsFour = inputDetailsFour.value;console.log(DetailsFour)});
+
+        inputDetailsFive.addEventListener('blur', (event) => {const DetailsFive = inputDetailsFive.value;console.log(DetailsFive)});
+
+        inputDetailsSix.addEventListener('blur', (event) => {const DetailsSix = inputDetailsSix.value;console.log(DetailsSix)});
+
+        inputDetailsSeven.addEventListener('blur', (event) => {const DetailsSeven = inputDetailsSeven.value;console.log(DetailsSeven)});
+
+        inputDetailsEight.addEventListener('blur', (event) => {const DetailsEight = inputDetailsEight.value;console.log(DetailsEight)});
+
+        inputDetailsNine.addEventListener('blur', (event) => {const DetailsNine = inputDetailsNine.value;console.log(DetailsNine)});
+
+        inputDetailsTen.addEventListener('blur', (event) => {const DetailsTen = inputDetailsTen.value;console.log(DetailsTen)});
+        
+        
+
